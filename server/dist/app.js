40c1e133b192b8c3f44b35dfe576fad5673e3711
Kaushik Mahadevan <kaushik.mahadevan@gmail.com>
2023-01-31 15:03:27 -0500

    added dotenv

--- server/dist/app.js
+++ server/dist/app.js
@@ -1,19 +1,53 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv = __importStar(require("dotenv"));
 const express_1 = __importDefault(require("express"));
+const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const cors_1 = __importDefault(require("cors"));
 const helmet_1 = __importDefault(require("helmet"));
+dotenv.config();
 const app = (0, express_1.default)();
+const PORT = process.env.PORT;
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: false }));
 app.use((0, cors_1.default)());
 app.use((0, helmet_1.default)());
-app.use(express_1.default.json());
+app.use((0, cookie_parser_1.default)());
 app.get('/', (req, res) => {
+    // this is how you get access to the request body, similarly can get access to route params and cookies etc
     console.log(`Hello ${JSON.stringify(req.body)}`);
+    // to access protected values from process.env (API_KEY or DB secret)
+    // the .env file holds all of these values and it is NOT committed to github because that would be dangerous
+    console.log(process.env.TEST_VAL);
+    // this is how an express route sends data back, although this is a really simple case - usually it sends some JSON
     res.send('hello');
 });
-app.listen(3000, () => {
-    console.log('listening on port 3000');
+app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`);
 });
--- server/src/app.ts
+++ server/src/app.ts
@@ -1,19 +1,31 @@
+import * as dotenv from 'dotenv';
+
 import express, { NextFunction, Request, Response } from 'express';
 
+import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import helmet from 'helmet';
 
+dotenv.config();
 const app = express();
+const PORT = process.env.PORT;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 app.use(helmet());
-app.use(express.json());
+app.use(cookieParser());
 
 app.get('/', (req: Request, res: Response) => {
+  // this is how you get access to the request body, similarly can get access to route params and cookies etc
   console.log(`Hello ${JSON.stringify(req.body)}`);
+  // to access protected values from process.env (API_KEY or DB secret)
+  // the .env file holds all of these values and it is NOT committed to github because that would be dangerous
+  console.log(process.env.TEST_VAL);
+  // this is how an express route sends data back, although this is a really simple case - usually it sends some JSON
   res.send('hello');
 });
 
-app.listen(3000, () => {
-  console.log('listening on port 3000');
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
 });
