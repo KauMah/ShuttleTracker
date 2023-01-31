d8739b2bf2efd31d9e23f8850f9bb209fec275ff
Kaushik Mahadevan <kaushik.mahadevan@gmail.com>
2023-01-31 17:39:36 -0500

    oopsie

--- server/src/app.ts
+++ server/src/app.ts
@@ -9,7 +9,7 @@ import users from './routes/users';
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
