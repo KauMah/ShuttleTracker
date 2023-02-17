894aaf9d3569950a44d5f20ec2f5afd8518103f9
Bryson <brysonmonteiro77@gmail.com>
2023-02-17 14:19:15 -0500

    Started the login page frontend

---
+++ client/src/components/Login/component.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const styles = {};
+// const login= () : JSX.Element {};
+const login = () => {
+  return <div>hello</div>;
+};
+export default login;
---
+++ client/src/components/Login/index.ts
@@ -0,0 +1,3 @@
+import login from './component';
+
+export default login;
