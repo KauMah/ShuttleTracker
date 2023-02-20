fbcb1266eac8484f6b393dcf47ca4bd7299571a2
Kaushik Mahadevan <kaushik.mahadevan@gmail.com>
2023-02-20 11:37:59 -0500

    cowboy part 2 - email validation

--- server/src/schemas/user.schema.ts
+++ server/src/schemas/user.schema.ts
@@ -5,7 +5,9 @@ import { Role } from '../models/user.model';
 export const registerUserSchema = object({
   body: object({
     name: string({ required_error: 'Name is required' }),
-    email: string({ required_error: 'email is required' }).email('Invalid email'),
+    email: string({ required_error: 'email is required' })
+      .email('Invalid email')
+      .regex(new RegExp('^[a-zA-Z0-9._%+-]+@montclair.edu$'), 'Invalid email domain'),
     password: string({ required_error: 'Password is required' })
       .min(8, 'Password must be longer than 8 characters')
       .max(32, 'Password must be shorter than 32 characters'),
@@ -22,7 +24,7 @@ export const registerUserSchema = object({
 
 export const loginUserSchema = object({
   body: object({
-    email: string({ required_error: 'Email is required' }).email('Invlaid email'),
+    email: string({ required_error: 'Email is required' }).email('Invalid email'),
     password: string({ required_error: 'Password is required' }).min(8, 'Invalid password'),
   }),
 });
