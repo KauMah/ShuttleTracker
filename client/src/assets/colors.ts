3ea9684a9d9d187b6707837ae5db211f685b3fff
Bryson <brysonmonteiro77@gmail.com>
2023-03-27 21:37:39 -0400

    tweaking the nav

--- client/src/assets/colors.ts
+++ client/src/assets/colors.ts
@@ -1,4 +1,5 @@
 export const $red = 'rgb(141, 34, 18)';
+export const $msured = '#D1190D';
 export const $white = '#fff';
 export const $black = '#000';
 export const $grey = '#888';
--- client/src/components/navBar/component.tsx
+++ client/src/components/navBar/component.tsx
@@ -1,52 +1,69 @@
-import { $grey, $red, $white } from '../../assets/colors';
+import { $black, $grey, $red, $white } from '../../assets/colors';
 
 import { NavLink } from 'react-router-dom';
 import { css } from '@emotion/react';
 
 const styles = {
-  background: {
+  msulogo: css({
+    backgroundColor: $white,
+    transform: 'skew(10deg)',
+    padding: '15px 30px',
+  }),
+  msulinks: css({
     backgroundColor: $red,
-    height: '25vh',
-    padding: '60px',
-  },
+    transform: 'skew(-14deg)',
+  }),
+  background: css({
+    backgroundColor: $red,
+    backdropFilter: 'blur(10px)',
+    WebKitBackDropFilter: 'blur(2px)',
+    mask: 'linear-gradient(black 30%, transparent)',
+    height: '15vh',
+    padding: '50px',
+  }),
   title: css({
-    color: $white,
-    fontSize: 50,
-    // borderBottom: '100px',
-    lineHeight: '0px',
+    color: $black,
+    fontSize: 40,
+    fontFamily: 'sans-serif',
+    fontWeight: 'Bold',
+    // fontStyle: 'oblique',
+    lineHeight: '50px',
+    textAlign: 'center',
     transition: 'color 0.25s',
     '&:hover': {
       color: $grey,
     },
   }),
-  links: {
-    fontSize: 20,
-    padding: '20px',
-    lineHeight: '0px',
-  },
+  links: css({
+    color: $black,
+    fontSize: 25,
+    fontWeight: 'bold',
+    padding: '30px 30px',
+    textAlign: 'center',
+  }),
 };
 
 const Navbar = () => {
   return (
     <div className="row">
-      <div className="col" style={styles.background}>
+      <div className="col-2" css={styles.msulogo}>
         <h1 css={styles.title}>MSU</h1>
       </div>
-      <div className="col" style={styles.background}>
+      <div className="col-10" css={styles.msulinks}>
         <div className="d-flex justify-content-end">
-          <NavLink to="/home" style={styles.links}>
+          <NavLink to="/home" css={styles.links}>
             Home
           </NavLink>
-          <NavLink to="/shuttleInfo" style={styles.links}>
-            View Shuttle-Info
+          <NavLink to="/shuttleInfo" css={styles.links}>
+            Shuttle
           </NavLink>
-          <NavLink to="/help" style={styles.links}>
+          <NavLink to="/help" css={styles.links}>
             Help
           </NavLink>
-          <NavLink to="/account" style={styles.links}>
+          <NavLink to="/account" css={styles.links}>
             Account
           </NavLink>
-          <NavLink to="/logout" style={styles.links}>
+          <NavLink to="/logout" css={styles.links}>
             Logout
           </NavLink>
         </div>
