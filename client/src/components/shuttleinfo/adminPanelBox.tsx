89c4d51c807aec455344746c196283a678fac8d3
Nicholas Hoo <47373627+nicholashoo@users.noreply.github.com>
2023-04-03 15:29:33 -0400

    Added styling and got dropdowns working

--- client/src/components/shuttleinfo/adminPanelBox.tsx
+++ client/src/components/shuttleinfo/adminPanelBox.tsx
@@ -1,17 +1,54 @@
-import React from 'react';
+import { $red, $white } from '../../assets/colors';
+import React, { useState } from 'react';
 
 interface AdminPanelBoxProps {
-  title: string;
-  content: string;
+  options: { title: string; content: string[] }[];
 }
 
-const AdminPanelBox: React.FC<AdminPanelBoxProps> = ({ title, content }) => {
+const AdminPanelBox: React.FC<AdminPanelBoxProps> = ({ options }) => {
+  const [selectedOption, setSelectedOption] = useState(options[0]);
+
+  const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedTitle = event.target.value;
+    const newSelectedOption = options.find((option) => option.title === selectedTitle);
+    if (newSelectedOption) {
+      setSelectedOption(newSelectedOption);
+    }
+  };
+
+  const cardStyle = {
+    backgroundColor: $white,
+    borderColor: $red,
+  };
+
+  const textStyle = {
+    color: $red,
+  };
+
   return (
     <div className="col-12 col-sm-6 col-md-4">
-      <div className="card mb-3">
+      <div className="card mb-3" style={cardStyle}>
         <div className="card-body">
-          <h5 className="card-title">{title}</h5>
-          <p className="card-text">{content}</p>
+          <select
+            value={selectedOption.title}
+            onChange={handleSelectChange}
+            className="form-select mb-2"
+            style={textStyle}
+          >
+            {options.map((option, index) => (
+              <option key={index} value={option.title}>
+                {option.title}
+              </option>
+            ))}
+          </select>
+          {selectedOption.content.map((text, index) => (
+            <React.Fragment key={index}>
+              <span style={textStyle}>
+                {index + 1}. {text}
+              </span>
+              {index < selectedOption.content.length - 1 && <br />}
+            </React.Fragment>
+          ))}
         </div>
       </div>
     </div>
