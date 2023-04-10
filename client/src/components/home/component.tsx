bb72ef8703b4e2203a45426de47885997de4f5c7
Inzhagi <inzhagey@gmail.com>
2023-04-09 23:13:11 -0400

    Map is showing

--- client/src/components/home/component.tsx
+++ client/src/components/home/component.tsx
@@ -1,11 +1,13 @@
-<<<<<<< HEAD
 import 'mapbox-gl/dist/mapbox-gl.css';
 
+import { ComponentProps, SetStateAction, useState } from 'react';
 import Map, { Marker } from 'react-map-gl';
-import { SetStateAction, useState } from 'react';
 
 import MapEvent from 'react-map-gl';
 import Navbar from '../navBar';
+import { NavigationControl } from 'react-map-gl';
+import ReactMapGL from 'react-map-gl';
+import ViewState from 'react-map-gl';
 import dotenv from 'dotenv';
 import envConfig from '../../../env-config.json';
 import { viewport } from '@popperjs/core';
@@ -18,38 +20,29 @@ const Home = () => {
   const [viewState, setViewState] = useState({
     latitude: 40.864872,
     longitude: -74.1995669,
-    zoom: 8,
+    zoom: 14.7,
     bearing: 0,
     pitch: 0,
     padding: { top: 0, right: 0, bottom: 0, left: 0 },
   });
 
-  const onMove = (event: typeof MapEvent) => {
-    setViewState(event);
-  };
-=======
-import MsuNav from '../navBar';
-
-const Home = () => {
-  return <MsuNav />;
-};
->>>>>>> 9f5bd8b41a0500206bcb58fcd4c95565e382636a
+  const [map, setMap] = useState<null | mapboxgl.Map>(null);
 
   return (
     <>
       <Navbar />
       <div style={{ width: '100vw', height: '100vh' }}>
-        <Map
+        <ReactMapGL
           {...viewState}
-          onMove={onMove}
+          onMove={(event) => setViewState(event.viewState)}
           mapboxAccessToken={mapboxToken} // replace with your access token
           mapStyle="mapbox://styles/mapbox/streets-v11"
-          initialViewState={viewState}
+          onLoad={(event) => setMap(event.target)}
         >
           <Marker latitude={40.864872} longitude={-74.1995669}>
             <div>Marker here</div>
           </Marker>
-        </Map>
+        </ReactMapGL>
       </div>
     </>
   );
