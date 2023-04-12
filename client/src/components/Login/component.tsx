e82e01f7b82c5cb27cfa4dbc337c979dd159be8d
Brysonm77 <104327324+Brysonm77@users.noreply.github.com>
2023-04-12 03:01:57 -0400

    C login style (#15)
    * Login page updated(styled)
    
    * tweaked submit and forgot pass/create account
    
    * Responsive

--- client/src/components/Login/component.tsx
+++ client/src/components/Login/component.tsx
@@ -1,46 +1,119 @@
 /* eslint-disable react/no-unescaped-entities */
-import { $grey, $red, $white } from '../../assets/colors';
+import { $grey, $msured, $red, $white } from '../../assets/colors';
 
 import LoginForm from './loginForm';
 import { NavLink } from 'react-router-dom';
-import React from 'react';
 import { css } from '@emotion/react';
 
 const styles = {
   background: {
-    backgroundColor: $red,
     height: '100vh',
     width: '100vw',
-    padding: '40px',
+    padding: '80px',
   },
+  container: css({
+    background: $white,
+    height: '55vh',
+    width: '50%',
+    border: '1px solid red',
+    borderRadius: '50px',
+    alignItems: 'center',
+    textAlign: 'center' as const,
+    paddingTop: '55px',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    '@media (max-width: 765px)': {
+      width: '70%',
+    },
+    '@media (max-width: 500px)': {
+      width: '90%',
+    },
+  }),
   title: css({
-    color: $white,
-    fontSize: 50,
+    height: '15vh',
+    width: '20vw',
+    backgroundColor: $msured,
+    transform: 'skew(-10deg)',
     transition: 'color 0.25s',
+    border: '1px solid white',
+    boxShadow: '5px 5px 5px grey',
+    '@media (max-width: 1100px)': {
+      width: '25vw',
+    },
+    '@media (max-width: 830px)': {
+      width: '30vw',
+    },
+    '@media (max-width: 765px)': {
+      width: '100%',
+    },
+  }),
+  titleBus: css({
+    color: $white,
+    fontFamily: 'Helvetica',
+    fontSize: 30,
+    fontWeight: 700,
+    marginTop: '3vh',
+    textAlign: 'center',
+    transform: 'skew(5deg)',
     '&:hover': {
       color: $grey,
     },
+    '@media (max-width: 765px)': {
+      textAlign: 'center',
+    },
   }),
-  container: {
-    alignItems: 'center',
-    textAlign: 'center' as const,
-    width: '40%',
-    marginLeft: 'auto',
-    marginRight: 'auto',
-  },
+  titleLogin: css({
+    fontFamily: 'Helvetica',
+    fontSize: '8vh',
+    fontWeight: 550,
+    textAlign: 'center',
+    marginTop: '-5vh',
+    color: $msured,
+  }),
+  link: css({
+    textDecoration: 'none',
+    color: $msured,
+    '&:hover': {
+      color: $red,
+    },
+  }),
+  clickHere: css({
+    fontFamily: 'Helvetica',
+    fontSize: '15px',
+    fontWeight: 520,
+  }),
 };
 const Login = (): JSX.Element => {
   return (
-    <div style={styles.background}>
-      <h1 css={styles.title}>MSU</h1>
-      <div style={styles.container} className="container-">
-        <LoginForm />
+    <div>
+      <div className="row">
+        <div className="col-12" css={styles.title}>
+          <h1 css={styles.titleBus}>MSU Shuttle Tracker</h1>
+        </div>
       </div>
-      <p className="d-flex justify-content-center" style={{ marginTop: '2vh' }}>
-        {/* &nbsp; makes it so that it skips a space */}
-        Don't have an account?&nbsp;<NavLink to="/register">Click Here</NavLink>
-      </p>
-      <div />
+      <div style={styles.background}>
+        <h2 css={styles.titleLogin}>Login</h2>
+        <div css={styles.container} className="container">
+          <LoginForm />
+        </div>
+        <div css={styles.clickHere}>
+          <p className="d-flex justify-content-center" style={{ marginTop: '2vh' }}>
+            {/* &nbsp; makes it so that it skips a space */}
+            Forgot Password?&nbsp;
+            <NavLink to="/forgotPass" css={styles.link}>
+              Click Here
+            </NavLink>
+          </p>
+          <p className="d-flex justify-content-center" style={{ marginTop: '-3vh' }}>
+            {/* &nbsp; makes it so that it skips a space */}
+            Don't have an account?&nbsp;
+            <NavLink to="/register" css={styles.link}>
+              Click Here
+            </NavLink>
+          </p>
+        </div>
+        <div />
+      </div>
     </div>
   );
 };
