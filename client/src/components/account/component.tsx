0656b81e860da321a6b8d894b432b98366f077ab
Bryson <brysonmonteiro77@gmail.com>
2023-04-17 21:34:09 -0400

    connected to backend

--- client/src/components/account/component.tsx
+++ client/src/components/account/component.tsx
@@ -1,18 +1,56 @@
+import { $black, $lightGrey, $msured } from '../../assets/colors';
+import { useEffect, useState } from 'react';
+
 import MsuNav from '../navBar';
+import { api } from '../../utils/api';
 import { css } from '@emotion/react';
 
 const styles = {
-  background: css({
-    height: '',
+  infoBox: css({
+    background: $lightGrey,
+    height: '7vh',
+    width: '80vw',
+    marginTop: '5px',
+    border: '1px solid black',
+    display: 'flex',
+    justifyContent: 'space-between',
+  }),
+  text: css({
+    display: 'flex',
+    justifyContent: 'space-between',
   }),
 };
 const Account = () => {
+  const [data, setData] = useState({ email: '', password: '' });
+
+  useEffect(() => {
+    api
+      .get('/user/')
+      .then((response) => {
+        console.log('response', response.data);
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching user data: ${error.message');
+      });
+  }, []);
   return (
     <div style={{ marginTop: '100px' }}>
       <MsuNav />
       <h1>Account</h1>
       <div className="row">
-        <div className="col">hello</div>
+        <div className="col d-flex justify-content-center">
+          <div style={{ flexDirection: 'column' }}>
+            <div css={styles.infoBox}>
+              <div>Email</div>
+              <div className="col d-flex justify-content-end">{data.email}tessdt</div>
+            </div>
+            <div css={styles.infoBox}>
+              <div>Password </div>
+              <div className="col d-flex justify-content-end">{data.password}dsd</div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
