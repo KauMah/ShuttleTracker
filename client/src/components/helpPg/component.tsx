a06e5f03a10901a579d65719e8fb7c690fad071d
Dennis Alarcon <dennis@pop-os.localdomain>
2023-04-18 05:35:41 -0400

    updated art and css

--- client/src/components/helpPg/component.tsx
+++ client/src/components/helpPg/component.tsx
@@ -1,17 +1,24 @@
-import './DrawerDropdown.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { $black, $grey, $lightGrey, $white } from '../../assets/colors';
 
-import DrawerDropdown from './DrawerDropdown';
+import FAQImg from '../../assets/img/FAQImg-4.jpg';
 import MsuNav from '../navBar';
 import { css } from '@emotion/react';
 
 const styles = {
+  container: {
+    display: 'flex',
+    justifyContent: 'center',
+  },
   colLeft: {
     backgroundColor: $lightGrey,
     height: '100vh',
-    marginTop: '98px',
+    marginTop: '640px',
+  },
+  card: {
+    width: '100%',
+    marginBottom: '20px',
   },
   colRight: {
     backgroundColor: $white,
@@ -35,6 +42,51 @@ const styles = {
     //left: '0',
     width: '100%',
   }),
+  headerImage: css({
+    background: `url(${FAQImg}) no-repeat 50% 50%`,
+    width: '100%',
+    height: 'auto',
+    marginLeft: '-921px',
+    marginTop: '-900px',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundPosition: '50% 30%',
+    backgroundSize: 'cover',
+  }),
+  headerTitle: css({
+    position: 'absolute',
+    top: '30%',
+    left: '55%',
+    transform: 'translate(-50%, -50%)',
+    color: $white,
+    fontFamily: 'Helvetica',
+    fontSize: '5em',
+  }),
+  font: {
+    fontFamily: 'Helvetica',
+    fontSize: '1.3em',
+    fontWeight: '100',
+  },
+  bolderFont: {
+    fontFamily: 'Helvetica',
+    fontSize: '1.3em',
+    fontWeight: '600',
+  },
+  contactHeader: {
+    //display: 'flex',
+    sjustifyContent: 'space-between',
+    borderBottom: `1px solid ${$black}`,
+    marginBottom: '10px',
+    paddingBottom: '10px',
+    //textAlign: 'center',
+  },
+  contact: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    marginBottom: '10px',
+    paddingBottom: '10px',
+    borderBottom: `1px solid ${$grey}`,
+  },
 };
 
 const HelpPg = () => {
@@ -45,85 +97,127 @@ const HelpPg = () => {
         <div className="row">
           <div className="col-3" style={styles.colLeft}>
             <div css={styles.header}>
-              <h1>Contacts</h1>
+              <h1>Important</h1>
             </div>
-            <h4>Shuttle Services</h4>
-            <p>Location: 145 Clove Rd.</p>
-            <p>Phone: 973-655-3326</p>
-            <p>Fax: 973-655-4494</p>
-            <p>Email: shuttle@montlcair.edu</p>
-            <h4>Executive Director, Facilities Technology</h4>
-            <p>John Bonin</p>
-            <p>Location: 150 Clove Road</p>
-            <p>Phone: 973-655-7868</p>
-            <p>E-mail: boninj@montclair.edu</p>
-            <h4>Supervisor, Shuttle Services</h4>
-            <p>Cheryl Ofoha</p>
-            <p>Location: 35 Clove Road</p>
-            <p>Phone: 973-655-3326</p>
-            <p>E-mail: lebertc@montclair.edu</p>
-            <h4>Coordinator, Commuter Life</h4>
-            <p>Antonio Talamo</p>
-            <p>Phone: 973-655-3377</p>
-            <p>E-mail: talamoa@montclair.edu</p>
+            <div css={styles.header}>
+              <div style={styles.contactHeader}>
+                <h1>Contacts</h1>
+              </div>
+            </div>
+            <div>
+              <div css={styles.contactHeader}>
+                <h3>Shuttle Services</h3>
+                <div style={styles.font}>
+                  <p>Location: 145 Clove Rd.</p>
+                  <p>Phone: 973-655-3326</p>
+                  <p>Fax: 973-655-4494</p>
+                  <p>Email: shuttle@montlcair.edu</p>
+                </div>
+              </div>
+              <div css={styles.contactHeader}>
+                <h3>Executive Director, Facilities Technology</h3>
+                <h4>John Bonin</h4>
+                <div style={styles.font}>
+                  <p>Location: 150 Clove Road</p>
+                  <p>Phone: 973-655-7868</p>
+                  <p>E-mail: boninj@montclair.edu</p>
+                </div>
+              </div>
+              <div css={styles.contactHeader}>
+                <h3>Supervisor, Shuttle Services</h3>
+                <h4>Cheryl Ofoha</h4>
+                <div style={styles.font}>
+                  <p>Location: 35 Clove Road</p>
+                  <p>Phone: 973-655-3326</p>
+                  <p>E-mail: lebertc@montclair.edu</p>
+                </div>
+              </div>
+              <div css={styles.contactHeader}>
+                <h3>Coordinator, Commuter Life</h3>
+                <h4>Antonio Talamo</h4>
+                <div style={styles.font}>
+                  <p>Phone: 973-655-3377</p>
+                  <p>E-mail: talamoa@montclair.edu</p>
+                </div>
+              </div>
+            </div>
           </div>
           <div className="col-9" style={styles.colRight}>
-            <div css={styles.header}>
+            {/* <div css={styles.header}>
               <h1>FAQ</h1>
-            </div>
-            <div className="drawerContiner">
-              <DrawerDropdown title="Q. How often do the buses run?">
-                <p>A. Every 8-15 mintues during peak ours, and every 20-30 mintues during off peak hours.</p>
-              </DrawerDropdown>
-            </div>
-            <div>
-              <DrawerDropdown title="Q. How can I track the bus in real-time?">
-                <p>A. Navigate to the shuttle info page, find your shuttle, and you will see the information there.</p>
-              </DrawerDropdown>
+            </div> */}
+            <div className="container">
+              <div css={styles.headerImage}>
+                <img src="src/assets/img/FAQImg-4.jpg" alt="Header" />
+              </div>
+              <div css={styles.headerTitle}>
+                <p>Frequently Asked Questions</p>
+              </div>
             </div>
             <div>
-              <DrawerDropdown title="Q. Are there any restrictions on items or pets that can be brought onto the shuttle buses?">
-                <p>
-                  A. Yes! On weekends however, the shuttle operates from 8:00AM-1:00AM as opposed the usual
+              <div>
+                <p style={styles.bolderFont}>Q. How often do the buses run?</p>
+                <p style={styles.font}>
+                  A. Every 8-15 mintues during peak ours, and every 20-30 mintues during off peak hours.
+                </p>
+              </div>
+              <div>
+                <p style={styles.bolderFont}>Q. How can I track the bus in real-time?</p>
+                <p style={styles.font}>
+                  A. Navigate to the shuttle info page, find your shuttle, and you will see the information there.
+                </p>
+              </div>
+              <div>
+                <p style={styles.bolderFont}>
+                  Q. Are there any restrictions on items or pets that can be brought onto the shuttle buses?
+                </p>
+                <p style={styles.font}>
+                  A. Yes! On weekends however, the shuttle operates from 8:00AM-1:00AM as opposed to the usual
                   6:00AM-1:00AM on weekdays.
                 </p>
-              </DrawerDropdown>
-            </div>
-            <div className="drawerContiner">
-              <DrawerDropdown title="Q. How  accessible are the shuttle buses for individuals with disabilities?">
-                <p>A. Every 8-15 mintues during peak ours, and every 20-30 mintues during off peak hours.</p>
-              </DrawerDropdown>
-            </div>
-            <div>
-              <DrawerDropdown title="Q. Can non-students or non-faculty members use the shuttle system?">
-                <p>A. Navigate to the shuttle info page, find your shuttle, and you will see the information there.</p>
-              </DrawerDropdown>
-            </div>
-            <div>
-              <DrawerDropdown title="Q. Can a student or faculty member bring a visitor?">
-                <p>
-                  A. Yes! On weekends however, the shuttle operates from 8:00AM-1:00AM as opposed the usual
+              </div>
+              <div className="drawerContiner">
+                <p style={styles.bolderFont}>
+                  Q. How accessible are the shuttle buses for individuals with disabilities?
+                </p>
+                <p style={styles.font}>
+                  A. Every 8-15 mintues during peak ours, and every 20-30 mintues during off peak hours.
+                </p>
+              </div>
+              <div>
+                <p style={styles.bolderFont}>Q. Can non-students or non-faculty members use the shuttle system?</p>
+                <p style={styles.font}>
+                  A. Navigate to the shuttle info page, find your shuttle, and you will see the information there.
+                </p>
+              </div>
+              <div>
+                <p style={styles.bolderFont}>Q. Can a student or faculty member bring a visitor?</p>
+                <p style={styles.font}>
+                  A. Yes! On weekends however, the shuttle operates from 8:00AM-1:00AM as opposed to the usual
                   6:00AM-1:00AM on weekdays.
                 </p>
-              </DrawerDropdown>
-            </div>
-            <div className="drawerContiner">
-              <DrawerDropdown title="Q. What should I do if I left something on the shuttle?">
-                <p>A. Every 8-15 mintues during peak ours, and every 20-30 mintues during off peak hours.</p>
-              </DrawerDropdown>
-            </div>
-            <div>
-              <DrawerDropdown title="Q. How can I provide feedback or report an issue with the shuttle service?">
-                <p>A. Navigate to the shuttle info page, find your shuttle, and you will see the information there.</p>
-              </DrawerDropdown>
-            </div>
-            <div>
-              <DrawerDropdown title="Q. Does the shuttle operate on weekends?">
-                <p>
-                  A. Yes! On weekends however, the shuttle operates from 8:00AM-1:00AM as opposed the usual
+              </div>
+              <div className="drawerContiner">
+                <p style={styles.bolderFont}>Q. What should I do if I left something on the shuttle?</p>
+                <p style={styles.font}>
+                  A. Every 8-15 mintues during peak ours, and every 20-30 mintues during off peak hours.
+                </p>
+              </div>
+              <div>
+                <p style={styles.bolderFont}>
+                  Q. How can I provide feedback or report an issue with the shuttle service?
+                </p>
+                <p style={styles.font}>
+                  A. Navigate to the shuttle info page, find your shuttle, and you will see the information there.
+                </p>
+              </div>
+              <div>
+                <p style={styles.bolderFont}>Q. Does the shuttle operate on weekends?</p>
+                <p style={styles.font}>
+                  A. Yes! On weekends however, the shuttle operates from 8:00AM-1:00AM as opposed to the usual
                   6:00AM-1:00AM on weekdays.
                 </p>
-              </DrawerDropdown>
+              </div>
             </div>
           </div>
         </div>
@@ -131,40 +225,5 @@ const HelpPg = () => {
     </div>
   );
 };
+
 export default HelpPg;
-
-// import { IoIosArrowDown } from 'react-icons/io';
-// import { RiArrowDropDownLine } from 'react-icons/ri';
-// import { css } from '@emotion/react';
-// import { dropdownStyles } from './DrawerStyles';
-// import { useState } from 'react';
-
-// interface DrawerDropdownProps {
-//   items: string[];
-// }
-
-// export const DrawerDropdown: React.FC<DrawerDropdownProps> = ({ items }) => {
-//   const [isOpen, setIsOpen] = useState(false);
-
-//   const toggleDropdown = () => {
-//     setIsOpen(!isOpen);
-//   };
-
-//   return (
-//     <div className={dropdownStyles.container}>
-//       <button className={dropdownStyles.button} onClick={toggleDropdown}>
-//         Dropdown
-//         {isOpen ? <IoIosArrowDown /> : <RiArrowDropDownLine />}
-//       </button>
-//       {isOpen && (
-//         <ul className={dropdownStyles.dropdown}>
-//           {items.map((item) => (
-//             <li key={item} className={dropdownStyles.item}>
-//               {item}
-//             </li>
-//           ))}
-//         </ul>
-//       )}
-//     </div>
-//   );
-// };
