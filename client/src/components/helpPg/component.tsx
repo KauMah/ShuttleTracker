86e2f1dac494d0e812f96c7d7a4ef4ecf9b21e13
Dennis Alarcon <dennis@pop-os.localdomain>
2023-04-22 21:38:39 -0400

    red FAQ LMAO

--- client/src/components/helpPg/component.tsx
+++ client/src/components/helpPg/component.tsx
@@ -1,6 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { $black, $grey, $lightGrey, $msured, $white } from '../../assets/colors';
+import { $black, $flash, $grey, $lightGrey, $msured, $red, $salmon, $white } from '../../assets/colors';
+import React, { useState } from 'react';
 
 import FAQImg from '../../assets/img/FAQImg-4.jpg';
 import MsuNav from '../navBar';
@@ -13,8 +14,8 @@ const styles = {
   },
   colLeft: {
     backgroundColor: $white,
-    height: '100vh',
-    marginTop: '550px',
+    height: '50vh',
+    //marginTop: '580px',
     '@media (max-width: 906px)': {
       marginLeft: '30%',
     },
@@ -22,11 +23,16 @@ const styles = {
   card: {
     width: '100%',
     marginBottom: '20px',
-    border: '1px solid rgba(0,0,0,0.6)',
+    border: '1px solid rgba(0,0,0,0.4)',
     padding: '6px',
     margin: '10px',
     marginLeft: '-2px',
     borderRadius: '10px',
+  },
+  questionLine: {
+    borderTop: '1px solid rgba(0,0,0,0.6)',
+    marginTop: '20px',
+    marginBottom: '10px',
   },
   colRight: {
     backgroundColor: $white,
@@ -39,7 +45,7 @@ const styles = {
     textAlign: 'center',
     fontFamily: 'Helvetica',
     fontSize: '3em',
-    margin: '0px',
+    margin: '10px',
     padding: '0px',
     fontWeight: '700',
   }),
@@ -72,7 +78,7 @@ const styles = {
     top: '30%',
     left: '53%',
     transform: 'translate(-50%, -50%)',
-    color: $white,
+    color: $msured,
     fontFamily: 'Helvetica',
     fontSize: '5em',
   }),
@@ -80,11 +86,13 @@ const styles = {
     fontFamily: 'Helvetica',
     fontSize: '1.3em',
     fontWeight: '100',
+    padding: '10px',
+    color: $msured,
   },
   questionFont: {
     fontFamily: 'Helvetica',
     fontSize: '1.3em',
-    color: $msured,
+    color: $black,
   },
   personFont: {
     fontFamily: 'Helvetica',
@@ -137,8 +145,8 @@ const styles = {
   }),
   imageContainer: css({
     height: '542px',
-    width: '1983px',
-    marginLeft: '-346px',
+    width: '1985px',
+    marginLeft: '-345px',
     overflow: 'hidden',
     //border: '1px solid black',
   }),
@@ -148,7 +156,7 @@ const styles = {
     display: 'block',
     //position: 'absolute',
     top: '50%',
-    transform: 'translateY(-24%)',
+    transform: 'translateY(-34%)',
     opacity: '0.9',
   }),
   contactsContainer: css({
@@ -158,7 +166,7 @@ const styles = {
   }),
   contactCard: css({
     border: '1px solid rgba(0, 0, 0, 0.5)',
-    borderRadius: '5px',
+    borderRadius: '15px',
     padding: '20px',
     backgroundColor: '#fff',
     boxShadow: '0 2px 5px rgba(0, 0, 0, 0.1)',
@@ -172,9 +180,86 @@ const styles = {
     padding: '0 10%', // adjust the percentage as needed for the desired column size
     backgroundColor: $white,
   }),
+  button: css({
+    backgroundColor: $white,
+    border: 'none',
+    color: $msured,
+    //padding: '8px 16px',
+    textAlign: 'center',
+    textDecoration: 'none',
+    display: 'inline-block',
+    fontSize: '28.5px',
+    //margin: '4px 2px',
+    cursor: 'pointer',
+    borderRadius: '4px',
+  }),
+  questionButton: css({
+    //backgroundColor: $red,
+    border: 'none',
+    cursor: 'pointer',
+    outline: 'none',
+    textAlign: 'left',
+    width: '100rem',
+    padding: '10px',
+    fontFamily: 'Helvetica',
+    fontSize: '1.3em',
+    fontWeight: '400',
+  }),
+  arrow: {
+    marginLeft: '10px',
+    transition: '0.3s',
+  },
+  answerContainer: {
+    overflow: 'hidden',
+    maxHeight: 0,
+    transition: 'max-height 0.5s ease-out',
+  },
+  answerVisible: {
+    maxHeight: '200px',
+  },
+};
+
+const scrollToBottom = () => {
+  window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
+};
+
+const QuestionAnswer = ({ question, answer, updateExpandedHeight }) => {
+  const [isAnswerVisible, setIsAnswerVisible] = useState(false);
+
+  const toggleAnswer = () => {
+    const height = !isAnswerVisible ? answer.length * 1.1 : -answer.length * 1.1;
+    updateExpandedHeight(height);
+    setIsAnswerVisible(!isAnswerVisible);
+  };
+
+  return (
+    <div style={styles.questionLine}>
+      <button css={styles.questionButton} onClick={toggleAnswer}>
+        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+          {question}
+          <span
+            style={{
+              ...styles.arrow,
+              transform: isAnswerVisible ? 'rotate(180deg)' : 'rotate(0deg)',
+            }}
+          >
+            ^
+          </span>
+        </div>
+      </button>
+      <div style={{ ...styles.answerContainer, ...(isAnswerVisible ? styles.answerVisible : {}) }}>
+        <p style={styles.font}>{answer}</p>
+      </div>
+    </div>
+  );
 };
 
 const HelpPg = () => {
+  const [totalExpandedHeight, setTotalExpandedHeight] = useState(0);
+  const [expandedHeight, setExpandedHeight] = useState(0);
+  const updateExpandedHeight = (height) => {
+    setExpandedHeight(expandedHeight + height);
+  };
   return (
     <div>
       <MsuNav />
@@ -196,65 +281,58 @@ const HelpPg = () => {
               <div>
                 <div>
                   <p style={styles.faqHeadFont}>
-                    Please find answers to commonly asked questions about shuttle operation below:
+                    <span>
+                      Please find answers to commonly asked questions about shuttle operation below. If you cannot find
+                      the answer to your question here, please refer to the Important Contacts
+                      <button css={styles.button} onClick={scrollToBottom}>
+                        Below
+                      </button>
+                      {''}.
+                    </span>
                   </p>
                 </div>
-                <div style={styles.card}>
-                  <p style={styles.questionFont}>Q. How often do the buses run?</p>
-                  <p style={styles.font}>
-                    A. Every 8-15 mintues during peak ours, and every 20-30 mintues during off peak hours.
-                  </p>
-                </div>
-                <div style={styles.card}>
-                  <p style={styles.questionFont}>Q. How can I track the bus in real-time?</p>
-                  <p style={styles.font}>
-                    A. Navigate to the shuttle info page, find your shuttle, and you will see the information there.
-                  </p>
-                </div>
-                <div style={styles.card}>
-                  <p style={styles.questionFont}>
-                    Q. Are there any restrictions on items or pets that can be brought onto the shuttle buses?
-                  </p>
-                  <p style={styles.font}>
-                    A. Yes! On weekends however, the shuttle operates from 8:00AM-1:00AM as opposed to the usual
-                    6:00AM-1:00AM on weekdays.
-                  </p>
-                </div>
-                <div className="drawerContiner" style={styles.card}>
-                  <p style={styles.questionFont}>
-                    Q. How accessible are the shuttle buses for individuals with disabilities?
-                  </p>
-                  <p style={styles.font}>
-                    A. Every 8-15 mintues during peak ours, and every 20-30 mintues during off peak hours.
-                  </p>
-                </div>
-                <div style={styles.card}>
-                  <p style={styles.questionFont}>Q. Can non-students or non-faculty members use the shuttle system?</p>
-                  <p style={styles.font}>
-                    A. Navigate to the shuttle info page, find your shuttle, and you will see the information there.
-                  </p>
-                </div>
-                <div style={styles.card}>
-                  <p style={styles.questionFont}>Q. Can a student or faculty member bring a visitor?</p>
-                  <p style={styles.font}>
-                    A. Yes! On weekends however, the shuttle operates from 8:00AM-1:00AM as opposed to the usual
-                    6:00AM-1:00AM on weekdays.
-                  </p>
-                </div>
-                <div className="drawerContiner" style={styles.card}>
-                  <p style={styles.questionFont}>Q. What should I do if I left something on the shuttle?</p>
-                  <p style={styles.font}>
-                    A. Every 8-15 mintues during peak ours, and every 20-30 mintues during off peak hours.
-                  </p>
-                </div>
-                <div style={styles.card}>
-                  <p style={styles.questionFont}>
-                    Q. How can I provide feedback or report an issue with the shuttle service?
-                  </p>
-                  <p style={styles.font}>
-                    A. Navigate to the shuttle info page, find your shuttle, and you will see the information there.
-                  </p>
-                </div>
+                <QuestionAnswer
+                  question="Q. How often do the buses run?"
+                  answer="A. Every 8-15 mintues during peak ours, and every 20-30 mintues during off peak hours."
+                  updateExpandedHeight={updateExpandedHeight}
+                />
+                <QuestionAnswer
+                  question="Q. How can I track the bus in real-time?"
+                  answer="A. Navigate to the shuttle info page, find your shuttle, and you will see the information there."
+                  updateExpandedHeight={updateExpandedHeight}
+                />
+                <QuestionAnswer
+                  question="Q. Are there any restrictions on items or pets that can be brought onto the shuttle buses?"
+                  answer="A. Yes! On weekends however, the shuttle operates from 8:00AM-1:00AM as opposed to the usual
+                  6:00AM-1:00AM on weekdays."
+                  updateExpandedHeight={updateExpandedHeight}
+                />
+                <QuestionAnswer
+                  question="Q. How accessible are the shuttle buses for individuals with disabilities?"
+                  answer="A. Every 8-15 mintues during peak ours, and every 20-30 mintues during off peak hours."
+                  updateExpandedHeight={updateExpandedHeight}
+                />
+                <QuestionAnswer
+                  question="Q. Can non-students or non-faculty members use the shuttle system?"
+                  answer="A. Navigate to the shuttle info page, find your shuttle, and you will see the information there."
+                  updateExpandedHeight={updateExpandedHeight}
+                />
+                <QuestionAnswer
+                  question="Q. Can a student or faculty member bring a visitor?"
+                  answer="A. Yes! On weekends however, the shuttle operates from 8:00AM-1:00AM as opposed to the usual
+                  6:00AM-1:00AM on weekdays."
+                  updateExpandedHeight={updateExpandedHeight}
+                />
+                <QuestionAnswer
+                  question="Q. What should I do if I left something on the shuttle?"
+                  answer="A. Every 8-15 mintues during peak ours, and every 20-30 mintues during off peak hours."
+                  updateExpandedHeight={updateExpandedHeight}
+                />
+                <QuestionAnswer
+                  question="Q. How can I provide feedback or report an issue with the shuttle service?"
+                  answer="A. Navigate to the shuttle info page, find your shuttle, and you will see the information there."
+                  updateExpandedHeight={updateExpandedHeight}
+                />
                 <div style={styles.card}>
                   <p style={styles.questionFont}>Q. Does the shuttle operate on weekends?</p>
                   <p style={styles.font}>
@@ -265,17 +343,15 @@ const HelpPg = () => {
               </div>
             </div>
             <div className="row">
-              <div className="col-12" style={styles.colLeft}>
-                <div css={styles.header}>
-                  <p>Important</p>
-                </div>
+              <div className="col-12" style={{ ...styles.colLeft, marginTop: 250 + expandedHeight }}>
                 <div css={styles.header}>
                   <div style={styles.contactHeader}>
-                    <p>Contacts</p>
+                    <p> Important Contacts</p>
                   </div>
                 </div>
                 <div className="container-fluid">
                   <div className="row">
+                    <div css={styles.contactsContainer}></div>
                     <div css={styles.contactRow}>
                       <div css={[styles.contactHeader, styles.contactCard]}>
                         <p style={styles.contactHeadFont}>Shuttle Services</p>
@@ -287,7 +363,7 @@ const HelpPg = () => {
                         </div>
                       </div>
                       <div css={[styles.contactHeader, styles.contactCard]}>
-                        <p style={styles.contactHeadFont}>Executive Director, Facilities Technology</p>
+                        <p style={styles.contactHeadFont}>Executive Director, Facilities {/*Technology*/}</p>
                         <p style={styles.execFont}>John Bonin</p>
                         <div style={styles.contactBodyFont}>
                           <p>Location: 150 Clove Road</p>
