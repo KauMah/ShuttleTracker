6cde9c5262869314cdc9839ed10ade4b514b09a9
Nicholas Hoo <47373627+nicholashoo@users.noreply.github.com>
2023-05-04 18:52:46 -0400

    first commit :D

---
+++ client/src/components/shuttlebusop/component.tsx
@@ -0,0 +1,11 @@
+import { Admin, Bus, Operator, Rider, Route, Stop } from '../shuttleinfo/component';
+import { useEffect, useState } from 'react';
+
+import { $msured } from '../../assets/colors';
+import AdminPanelBox from '../shuttleinfo/adminPanelBox';
+import MsuNav from '../navBar';
+import { api } from '../../utils/api';
+
+const ShuttleBusop = () => {};
+
+export default ShuttleBusop;
