1ea8fc72d557eeed544585a63fccaac297ac3e86
Kaushik Mahadevan <kaushik.mahadevan@gmail.com>
2023-05-05 10:52:26 -0400

    close enough

--- client/src/components/home/component.tsx
+++ client/src/components/home/component.tsx
@@ -1,94 +1,174 @@
 import 'mapbox-gl/dist/mapbox-gl.css';
-import './styles.css';
 
-import { ComponentProps, SetStateAction, useRef, useState } from 'react';
-import Map, { Marker } from 'react-map-gl';
+import { $grey, $msured, $red, $white } from '../../assets/colors';
+import ReactMapGL, { Layer, MapRef, Marker, Source } from 'react-map-gl';
+import { useEffect, useRef, useState } from 'react';
 
 import AlertModal from './alertModal';
-import MapEvent from 'react-map-gl';
-import MapboxViewport from 'react-map-gl';
 import Navbar from '../navBar';
-import { NavigationControl } from 'react-map-gl';
-import ReactMapGL from 'react-map-gl';
-import RouterInfo from './viewRoutesInfo';
-import ShuttleInfo from './viewShuttleInfo';
-import StopInfo from './viewStopsInfo';
-import ViewRoutes from './viewRoutesInfo';
-import ViewState from 'react-map-gl';
+import _ from 'lodash';
+import { api } from '../../utils/api';
 import bus from './bus.png';
-import dotenv from 'dotenv';
 import envConfig from '../../../env-config.json';
-import map from './map.png';
-import mapboxgl from 'mapbox-gl';
-import { viewport } from '@popperjs/core';
 
 const mapboxToken = envConfig.REACT_APP_MAPBOX_TOKEN;
 
-//dotenv.config();
+const styles = {
+  container: {
+    marginTop: 200,
+  },
+  route: {
+    backgroundColor: $grey,
+    textAlign: 'center' as const,
+    color: $white,
+    cursor: 'pointer',
+  },
+  stop: {
+    backgroundColor: $grey,
+    textAlign: 'center' as const,
+    color: $white,
+    cursor: 'pointer',
+  },
+  map: {
+    width: '100%',
+    height: 600,
+    marginBottom: 200,
+    marginTop: 30,
+  },
+};
 
 const Home = () => {
-  const [viewState, setViewState] = useState({
+  const [routes, setRoutes] = useState<any>([]);
+  const [route, setRoute] = useState();
+  const [stops, setStops] = useState([]);
+  const [stop, setStop] = useState();
+  const [waypoints, setWaypoints] = useState();
+  const [shuttles, setShuttles] = useState([]);
+  const map = useRef<MapRef>(null);
+
+  const mapPos = {
     latitude: 40.8652598,
     longitude: -74.199567,
     zoom: 14,
     bearing: 0,
     pitch: 0,
     padding: { top: 0, right: 0, bottom: 0, left: 0 },
-  });
-
-  const stopCoordinates = [
-    { latitude: 40.8720293, longitude: -74.1987873, label: 'Stop 1' },
-    { latitude: 40.8705887, longitude: -74.1988991, label: 'Stop 2' },
-    { latitude: 40.870515, longitude: -74.2010166, label: 'Stop 3' },
-    { latitude: 40.870515, longitude: -74.2013492, label: 'Stop 4' },
-    { latitude: 40.8690749, longitude: -74.1996545, label: 'Stop 5' },
-    { latitude: 40.8676239, longitude: -74.2006334, label: 'Stop 6' },
-    { latitude: 40.867362, longitude: -74.2002158, label: 'Stop 7' },
-    { latitude: 40.8650461, longitude: -74.1982911, label: 'Stop 8' },
-    { latitude: 40.8668351, longitude: -74.1965947, label: 'Stop 9' },
-    { latitude: 40.8610319, longitude: -74.2000521, label: 'Stop 10' },
-    { latitude: 40.8595465, longitude: -74.2000869, label: 'Stop 11' },
-  ];
-
-  const [showMap, setShowMap] = useState(false);
-  const mapRef = useRef<HTMLDivElement>(null);
-  const handleViewMapClick = () => {
-    setShowMap(true);
-    if (mapRef.current) {
-      mapRef.current.scrollIntoView({ behavior: 'smooth' });
-    }
   };
+
+  // fetch Routes (I am .thenning because oh my god are we pressed for time)
+  useEffect(() => {
+    if (routes.length < 1) {
+      api.get('/route/').then((data) => {
+        setRoutes(data.data.data);
+      });
+    }
+  }, [routes]);
+
+  useEffect(() => {
+    const res = api.get('/shuttle').then((data) => {
+      const buses = data.data.data;
+      if (!route) return;
+      const rtBus = buses.filter((b: any) => route._id === b.route);
+      setShuttles(rtBus);
+    });
+  }, [route]);
+
+  useEffect(() => {
+    let coordStr = '';
+    if (stops.length === 0) return;
+    stops.map((st, index) => {
+      coordStr += `${index === 0 ? '' : ';'}${_.get(st, 'loc.coordinates[0]', '')},${_.get(
+        st,
+        'loc.coordinates[1]',
+        ''
+      )}`;
+    });
+    const query = `https://api.mapbox.com/directions/v5/mapbox/driving/${coordStr}?geometries=geojson&access_token=${mapboxToken}`;
+    const fetcher = async () => {
+      const res = await fetch(query);
+      const data = await res.json();
+      const rt = data.routes[0].geometry.coordinates;
+      const geojson = {
+        type: 'Feature',
+        geometry: {
+          type: 'LineString',
+          coordinates: rt,
+        },
+      };
+      setWaypoints(geojson);
+    };
+    fetcher();
+  }, [stops]);
 
   return (
     <>
       <Navbar />
-      <div className="view-map-container">
-        <a className="view-map-link" onClick={handleViewMapClick}>
-          View Map
-        </a>
-        <img src={map} alt="Map" className="map-image" />
-      </div>
-      <AlertModal />
-      <div>
-        <StopInfo />
-      </div>
-      <div>
-        <ViewRoutes />
-      </div>
-      <div>
-        <ShuttleInfo />
-      </div>
-      <div className="map-container">
-        <div ref={mapRef} className="react-map-gl d-flex justify-content-center">
-          <ReactMapGL {...viewState} mapboxAccessToken={mapboxToken} mapStyle="mapbox://styles/mapbox/streets-v11">
-            {stopCoordinates.map((stop) => (
-              <Marker key={stop.label} latitude={stop.latitude} longitude={stop.longitude}>
-                <img src={bus} alt="Bus stop icon" width={30} height={30} />
-                <div className="marker">{stop.label}</div>
+      <div className="container" style={styles.container}>
+        <AlertModal />
+        <div className="row">
+          <h3>Routes</h3>
+          {routes.length > 0 &&
+            routes.map((rt: any) => (
+              <div
+                style={styles.route}
+                className="col-12 col-sm-4 col-md-3"
+                onClick={() => {
+                  setRoute(rt);
+                  setStops(rt.stops);
+                }}
+                key={`route-${rt._id}`}
+              >
+                {rt.name}
+              </div>
+            ))}
+        </div>
+        {stops && (
+          <>
+            <h3>Stops</h3>
+            <div className="row">
+              {stops.map((st: any) => (
+                <div key={`stop-${st.name}`} style={styles.stop} className="col-12 col-sm-4 col-md-3">
+                  {st.name}
+                </div>
+              ))}
+            </div>
+          </>
+        )}
+        <ReactMapGL
+          {...mapPos}
+          mapboxAccessToken={mapboxToken}
+          mapStyle="mapbox://styles/mapbox/streets-v11"
+          style={styles.map}
+          ref={map}
+        >
+          {shuttles &&
+            shuttles.map((st: any) => (
+              <Marker
+                key={`marker-${st._id}`}
+                latitude={_.get(st, 'loc.coordinates[1]', 0)}
+                longitude={_.get(st, 'loc.coordinates[0]', 0)}
+              >
+                <div style={{ height: 20, width: 20, backgroundColor: $msured }}></div>
+                <div className="marker">{st.name}</div>
               </Marker>
             ))}
-          </ReactMapGL>
-        </div>
+          {stops &&
+            stops.map((st: any) => (
+              <Marker
+                key={`marker-${st.name}`}
+                latitude={_.get(st, 'loc.coordinates[1]', 0)}
+                longitude={_.get(st, 'loc.coordinates[0]', 0)}
+              >
+                <img src={bus} alt="Bus stop icon" width={30} height={30} />
+                <div className="marker">{st.name}</div>
+              </Marker>
+            ))}
+          {waypoints && (
+            <Source id="route" type="geojson" data={{ type: 'FeatureCollection', features: [waypoints] }}>
+              <Layer {...{ id: 'line', type: 'line', paint: { 'line-color': $red, 'line-width': 5 } }} />
+            </Source>
+          )}
+        </ReactMapGL>
       </div>
     </>
   );
